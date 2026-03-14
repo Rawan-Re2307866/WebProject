9f1aea1aafbb832fde44dceb609b76b9b80ac066
asmaadawas <ad2203617@qu.edu.qa>
2026-03-14 05:51:26 +0300

    added post menu

--- CMPS350_Project/scripts/menudisplay.js
+++ CMPS350_Project/scripts/menudisplay.js
@@ -2,13 +2,12 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuCloseBtn = document.querySelector(".menu-close-btn");
 const menuBar = document.querySelector(".menu-bar");
-const hideContent = document.querySelector(".hide-content");
 
 menuBtn.addEventListener('click', () => { 
     menuBar.classList.add('display');
-    hideContent.classList.add('hide');
+    menuCloseBtn.classList.add('close');
 });
 menuCloseBtn.addEventListener('click', () => {
     menuBar.classList.remove('display');
-    hideContent.classList.remove('hide');
+    menuCloseBtn.classList.remove('close');
 });
