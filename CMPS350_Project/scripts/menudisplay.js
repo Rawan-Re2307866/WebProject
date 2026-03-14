09e5df423bad37fb81969f77b85bdf9b55ff5e5e
asmaadawas <ad2203617@qu.edu.qa>
2026-03-14 03:06:34 +0300

    update menu action

--- CMPS350_Project/scripts/menudisplay.js
+++ CMPS350_Project/scripts/menudisplay.js
@@ -2,6 +2,13 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuCloseBtn = document.querySelector(".menu-close-btn");
 const menuBar = document.querySelector(".menu-bar");
+const hideContent = document.querySelector(".hide-content");
 
-menuBtn.addEventListener('click', () => menuBar.classList.add('display'));
-menuCloseBtn.addEventListener('click', () => menuBar.classList.remove('display'));
+menuBtn.addEventListener('click', () => { 
+    menuBar.classList.add('display');
+    hideContent.classList.add('hide');
+});
+menuCloseBtn.addEventListener('click', () => {
+    menuBar.classList.remove('display');
+    hideContent.classList.remove('hide');
+});
