ceaf4caa169ce17d1433449cb4bada5323071ab1
Rawan-Re2307866 <re2307866@qu.edu.qa>
2026-05-02 17:45:57 +0300

    Update prisma.js

--- CMPS350_Phase_two/phase-two/repos/prisma.js
+++ CMPS350_Phase_two/phase-two/repos/prisma.js
@@ -1,9 +1,10 @@
-import { PrismaLibSql } from "@prisma/adapter-libsql";
-import { PrismaClient } from "../prisma/client/index.js";
 
-export default new PrismaClient({
-  adapter: new PrismaLibSql({
-    url: process.env.DATABASE_URL ?? "",
-  }),
-  log: ["query"],
-});
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis;
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
